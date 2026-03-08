8de00a9252debbedf4dd1f8ca66b02032b603655
Arming926 <kelvin20040926@gmail.com>
2026-03-08 16:32:30 +0800

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,27 @@
+class User:
+    def init(self, username, password, nickname):
+        self.username = username
+        self.password = password
+        self.nickname = nickname
+        self.group = None
+
+    def get_nickname(self):
+        return self.nickname
+
+    def get_username(self):
+        return self.username
+
+    def check_password(self, pwd):
+        return self.password == pwd
+
+    def set_group(self, group):
+        self.group = group
+
+    def get_group(self):
+        return self.group
+
+    def show_status(self):
+        if self.group is None:
+            return f"{self.nickname}   Still Empty..."
+        else:
+            return f"{self.nickname}   {self.group.get_name()}   {self.group.get_current()}/{self.__group.get_limit()}"
